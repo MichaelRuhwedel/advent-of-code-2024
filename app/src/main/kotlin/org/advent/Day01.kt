b6e9f15af11a34ebf81995f19d78d3b3e3ae802b
Michael Ruhwedel <michael.ruhwedel@lightspeedhq.com>
2024-12-02 00:02:48 +0100

    prep day02

--- app/src/main/kotlin/org/advent/Day01.kt
+++ app/src/main/kotlin/org/advent/Day01.kt
@@ -7,3 +7,18 @@ val answer01: (Pair<List<Int>, List<Int>>) -> Int = { (ls, rs) ->
     { l, r -> abs(l - r) }
         .reduce(Int::plus)
 }
+
+val answer02: (Pair<List<Int>, List<Int>>) -> Int = { (ls, rs) ->
+    rs
+        .fold(mutableMapOf<Int, Int>())
+        { acc, i ->
+            acc[i] = acc.getOrDefault(i, 0) + 1
+            acc
+        }
+        .let {
+            ls.map { l ->
+                l * it.getOrDefault(l, 0)
+            }
+                .reduce(Int::plus)
+        }
+}
