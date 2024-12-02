d93146c294d3a55a3906c437d08c6dd43be92223
Michael Ruhwedel <michael.ruhwedel@lightspeedhq.com>
2024-12-03 00:50:09 +0100

    cleanup

--- app/src/main/kotlin/org/advent/Day02.kt
+++ app/src/main/kotlin/org/advent/Day02.kt
@@ -3,33 +3,43 @@ package org.advent
 import kotlin.math.abs
 
 
-fun day02(input: String = "02.txt"): List<Long> =
-    listOf(
-        Util.readLines(input)
-            .map { it.split(' ').map(String::toInt) }
-            .filter(::isSafe)
-            .count()
+fun day02(input: String = "02.txt"): List<Int> {
+    val lines = Util.readLines(input)
+        .map { it.split(' ').map(String::toInt) }
+        .toList()
+    return listOf(
+        lines
+            .count(::isSafe),
+        lines.count { levels ->
+            isSafe(levels) ||
+                    (0..levels.size)
+                        .any { isSafe(levels.filterIndexed { index, _ -> index != it }) }
+        }
+
     )
+}
 
 private fun isSafe(list: List<Int>): Boolean {
     var ascending: Boolean? = null
-    return list
-        .windowed(2)
-        .all { (current, next) ->
-            if (abs(next - current) !in 1..3) return false
-            when {
-                current < next ->
-                    if (ascending == false)
-                        return false
-                    else
-                        ascending = true
+    for (i in 0 until list.size - 1) {
+        val current = list[i]
+        val next = list[i + 1]
 
-                next < current ->
-                    if (ascending == true)
-                        return false
-                    else
-                        ascending = false
-            }
-            true
+        if (abs(next - current) !in 1..3) return false
+
+        when {
+            current < next ->
+                if (ascending == false)
+                    return false
+                else
+                    ascending = true
+
+            next < current ->
+                if (ascending == true)
+                    return false
+                else
+                    ascending = false
         }
+    }
+    return true
 }
--- app/src/test/kotlin/org/advent/Day02Test.kt
+++ app/src/test/kotlin/org/advent/Day02Test.kt
@@ -7,6 +7,6 @@ import kotlin.test.assertEquals
 class Day02Test {
     @Test
     fun day02Test() {
-        assertEquals(listOf(2L), day02("02-test.txt"))
+        assertEquals(listOf(2,4), day02("02-test.txt"))
     }
 }
