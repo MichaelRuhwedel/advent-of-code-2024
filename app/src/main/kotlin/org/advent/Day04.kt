e8f278fba74e42087db6beb0c98932de8458e8c4
Michael Ruhwedel <michael.ruhwedel@lightspeedhq.com>
2024-12-08 12:29:35 +0100

    improve day03

--- app/src/main/kotlin/org/advent/Day04.kt
+++ app/src/main/kotlin/org/advent/Day04.kt
@@ -1,14 +1,14 @@
 package org.advent
 
-val xmas = listOf('X', 'M', 'A', 'S')
-var xmasVariations = setOf(xmas, xmas.reversed())
+internal val xmas = listOf('X', 'M', 'A', 'S')
+internal val xmasVariations = setOf(xmas, xmas.reversed())
 
-val mas = listOf('M', 'A', 'S')
-val masVariations = setOf(mas, mas.reversed())
+private val mas = listOf('M', 'A', 'S')
+private val masVariations = setOf(mas, mas.reversed())
 
-val wordLength = xmas.size
-val wls = wordLength - 1
-const val red = "\u001b[31m-\u001B[0m"
+private val wordLength = xmas.size
+private val contunuation = wordLength - 1
+private const val red = "\u001b[31m-\u001B[0m"
 
 fun day04(input: String = "04.txt"): List<Int> {
 
@@ -21,7 +21,7 @@ private fun answer01(chars: Array<CharArray>): Int {
     chars.forEachIndexed { i, row ->
         row.forEachIndexed { j, c ->
             var mark = false
-            if (c == xmas.first() || c == xmas.last()) {
+            if (c in setOf(xmas.first(), xmas.last())) {
                 if (chars.isHorizontal(j, i)) {
                     mark = true
                     count++
@@ -50,7 +50,7 @@ private fun answer02(chars: Array<CharArray>): Int {
     var count = 0
     chars.forEachIndexed { i, row ->
         row.forEachIndexed { j, c ->
-            if (c == 'A' && chars.isMas(i, j)) {
+            if (c == mas[1] && chars.isMas(i, j)) {
                 print(red)
                 count++
             } else {
@@ -64,8 +64,8 @@ private fun answer02(chars: Array<CharArray>): Int {
 
 
 private fun Array<CharArray>.isDiagonalR(i: Int, j: Int) =
-    i + wls < size && j + wls < this[i].size &&
-            (0..wls)
+    i + contunuation < size && j + contunuation < this[i].size &&
+            (0..contunuation)
                 .map { this[i + it][j + it] }
                 .toList()
                 .let(xmasVariations::contains)
@@ -76,14 +76,14 @@ private fun Array<CharArray>.isMas(i: Int, j: Int) =
             listOf(this[i - 1][j + 1], this[i][j], this[i + 1][j - 1]) in masVariations
 
 private fun Array<CharArray>.isDiagonalL(i: Int, j: Int) =
-    i + wls < size && 0 <= j - wls &&
-            (0..wls)
+    i + contunuation < size && 0 <= j - contunuation &&
+            (0..contunuation)
                 .map { this[i + it][j - it] }
                 .toList()
                 .let(xmasVariations::contains)
 
 private fun Array<CharArray>.isVertical(i: Int, j: Int) =
-    i + wls < size && (i..<i + xmas.size).map { this[it][j] }.toList() in xmasVariations
+    i + contunuation < size && (i..<i + xmas.size).map { this[it][j] }.toList() in xmasVariations
 
 private fun Array<CharArray>.isHorizontal(j: Int, i: Int) =
     j + wordLength <= this[i].size &&
