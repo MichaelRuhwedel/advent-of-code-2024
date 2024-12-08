b135ad25c17bbf2e094843fbb1ed4d33137f1e62
Michael Ruhwedel <michael.ruhwedel@lightspeedhq.com>
2024-12-08 19:48:10 +0100

    clean dayˆ

--- app/src/main/kotlin/org/advent/Day06.kt
+++ app/src/main/kotlin/org/advent/Day06.kt
@@ -9,47 +9,18 @@ enum class Guard(val c: Char) {
 fun day06(input: String = "06.txt"): List<Int> {
     val map = Util.readMatrix(input)
 
-    var guardPos = map.findGuard()
-    println("Guard at ${guardPos}")
-    val visited = mutableSetOf(guardPos.first)
-    while (map.inMap(guardPos.first)) {
-        val gIJ = guardPos.first
-        val direction = guardPos.second
-        gIJ.let { (i, j) ->
-            visited += gIJ
-            guardPos = when (direction) {
-                NORTH -> {
-                    if (map.isWall(i - 1, j)) {
-                        gIJ to EAST
-                    } else {
-                        (i - 1 to j) to NORTH
-                    }
-                }
-
-                SOUTH -> {
-                    if (map.isWall(i + 1, j)) {
-                        gIJ to WEST
-                    } else {
-                        (i + 1 to j) to SOUTH
-                    }
-                }
-
-
-                WEST -> {
-                    if (map.isWall(i, j - 1)) {
-                        gIJ to NORTH
-                    } else {
-                        (i to j - 1) to WEST
-                    }
-                }
-
-                EAST -> {
-                    if (map.isWall(i, j + 1)) {
-                        gIJ to SOUTH
-                    } else {
-                        (i to j + 1) to EAST
-                    }
-                }
+    var guard = map.findGuard()
+    val visited = mutableSetOf<Pair<Int, Int>>()
+    while (map.inMap(guard.first)) {
+        val currentPos = guard.first
+        val direction = guard.second
+        currentPos.let { (i, j) ->
+            visited += currentPos
+            guard = when (direction) {
+                NORTH -> goOrTurn(map, i - 1 to j, currentPos, EAST, direction)
+                SOUTH -> goOrTurn(map, i + 1 to j, currentPos, WEST, direction)
+                WEST -> goOrTurn(map, i to j - 1, currentPos, NORTH, direction)
+                EAST -> goOrTurn(map, i to j + 1, currentPos, SOUTH, direction)
             }
         }
 //        map.forEachIndexed { i, row ->
@@ -66,7 +37,19 @@ fun day06(input: String = "06.txt"): List<Int> {
     return listOf(visited.size)
 }
 
-private fun Array<CharArray>.isWall(i: Int, j: Int) = i in this.indices && j in this[i].indices && this[i][j] == '#'
+private fun goOrTurn(map: Array<CharArray>,
+    nextPos: Pair<Int, Int>,
+    currentPos: Pair<Int, Int>,
+    nextDirection: Guard,
+    oldDirection: Guard
+) = if (map.isWall(nextPos)) {
+    currentPos to nextDirection
+} else {
+    nextPos to oldDirection
+}
+
+private fun Array<CharArray>.isWall(ij: Pair<Int, Int>) =
+    ij.let { (i, j) -> i in indices && j in this[i].indices && this[i][j] == '#' }
 
 fun Array<CharArray>.inMap(guardPos: Pair<Int, Int>): Boolean = guardPos
     .let { (i, j) ->
